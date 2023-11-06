a184df8e8b639c5ba887cdb996dcb0f5eb2ef5c6
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-06 12:03:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let strongElement = document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
-
+	for ( let i = 0; i < strongElement.length; i++){
+		strongElement[i].classList.add("addColor");
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+for ( let i = 0; i < strongElement.length; i++){
+		strongElement[i].classList.remove("addColor");
+	}
     
 }
